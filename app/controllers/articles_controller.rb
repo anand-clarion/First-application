2976bdd1fc11084b00fcbedece4a24507db02e3b
Anand Tripathi <anand.tripathi@clariontechnologies.co.in>
2014-06-09 18:28:20 +0530

    article generation complete

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,21 +1,41 @@
 class ArticlesController < ApplicationController
+  def new
+    @article = Article.new
+  end
 
-	def index
-	end
+  def create
+    @article = current_user.articles.new(params[:article])
+    if @article.save
+      redirect_to @article
+    else
+      render 'new'
+    end
+  end  
 
-	def new
-	end
-	
-	def create
-	end
-	
-	def show
-	end
-	
-	def edit
-	end
-	
-	def destroy
-	end
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def index
+  	@article = Article.all
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update_attributes(params[:article])
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    Article.find(params[:id]).destroy
+    redirect_to articles_path
+  end
 
 end
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-  # attr_accessible :title, :body
+  belongs_to :user
+  attr_accessible :title, :description, :user_id
 end
